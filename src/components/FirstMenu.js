01214c72d65f7989053980e10c8f2410ba1df5d7
Somanyu <somanyu.03samal@gmail.com>
2023-01-22 10:02:30 +0530

    👽️ api: error handle if api is not working

--- src/components/FirstMenu.js
+++ src/components/FirstMenu.js
@@ -6,13 +6,42 @@ import { useState, useEffect } from 'react';
 
 function FirstMenu() {
     const [contacts, setContacts] = useState([]);
+    const [error, setError] = useState(null);
+    const [reload, setReload] = useState(false);
 
     useEffect(() => {
         fetch('http://localhost:3001/contact')
-          .then(res => res.json())
-          .then(data => setContacts(data));
-      }, []);
-      
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(res.statusText);
+                }
+                return res.json()
+            })
+            .then(data => setContacts(data))
+            .catch(err => setError(err.message));
+    }, []);
+
+    useEffect(() => {
+        if (reload) {
+          window.location.reload();
+        }
+      }, [reload]);
+
+    if (error) {
+        return (
+            <>
+                <div className='p-5'>
+                    <div id="alert-border-2" class="flex p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800" role="alert">
+                        <svg class="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                        <div class="ml-3 text-sm font-medium">
+                            {error}. Kindly <button onClick={() => setReload(true)} class="font-semibold underline hover:no-underline">reload</button> and try again or return to <a class="font-semibold underline hover:no-underline" href="/">home</a>.
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
     return (
         <>
             <h1 className="text-3xl font-bold text-black text-center">
